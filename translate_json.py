3dac3337a670fffef7496728366cf345ce87db96
gerardPlanella <gerard.plfo@gmail.com>
2023-05-01 15:13:19 +0200

    made translation script

--- translate_json.py
+++ translate_json.py
@@ -4,7 +4,24 @@ from typing import List
 from data import Language
 
 
+
+import os 
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] ="data\multilingualstereotypes-b8b5de5e15d6.json"
+
 def translate_json(input_path: str, output_path: str, input_lang: Language, output_lang: Language, ignored_words: List[str]):
+    lang_codes = {
+        Language.English: "en",
+        Language.Spanish: "es",
+        Language.French: "fr",
+        Language.Greek: "el",
+        Language.Croatian: "hr",
+        Language.Catalan: "ca"
+    }
+
+    input_lang_code = lang_codes[input_lang]
+    output_lang_code = lang_codes[output_lang]
+    
     # Set up the translation client
     translate_client = translate.Client()
 
@@ -12,53 +29,56 @@ def translate_json(input_path: str, output_path: str, input_lang: Language, outp
     with open(input_path, "r") as f:
         input_json = json.load(f)
 
-    # Define a function to apply the ignored words filter to a string
-    def ignore_words(text):
-        for word in ignored_words:
-            text = text.replace(word, f"<IGNORE_{word}>")
-        return text
-
-    # Define a function to remove the ignored words filter from a string
-    def unignore_words(text):
-        for word in ignored_words:
-            text = text.replace(f"<IGNORE_{word}>", word)
-        return text
-
     # Translate the prompts and items in the JSON file
     for category in input_json:
-        prompts = input_json[category]["prompts"]
-        items = input_json[category]["items"]
+        if category == "general_prompts":
+            # Translate the general prompts
+            prompts = input_json[category]
+            translated_prompts = []
+            for prompt in prompts:
+                #print(prompt)
+                translation = translate_client.translate(prompt, source_language=input_lang_code, target_language=output_lang_code)
+                translation = translation['translatedText'].encode('utf-8').decode('utf-8')
+                translated_prompts.append(translation)
 
-        # Translate the prompts
-        translated_prompts = []
-        for prompt in prompts:
-            ignored_prompt = ignore_words(prompt)
-            translation = translate_client.translate(ignored_prompt, source_language=input_lang.value, target_language=output_lang.value)
-            translated_prompt = unignore_words(translation["translatedText"])
-            translated_prompts.append(translated_prompt)
+            # Update the JSON file with the translated prompts
+            input_json[category] = translated_prompts
+        else:
+            # Translate the prompts and items for this category
+            prompts = input_json[category]["prompts"]
+            items = input_json[category]["items"]
 
-        # Translate the items
-        translated_items = []
-        for item in items:
-            ignored_item = ignore_words(item)
-            translation = translate_client.translate(ignored_item, source_language=input_lang.value, target_language=output_lang.value)
-            translated_item = unignore_words(translation["translatedText"])
-            translated_items.append(translated_item)
+            # Translate the prompts
+            translated_prompts = []
+            for prompt in prompts:
+                #print(prompt)
+                translation = translate_client.translate(prompt, source_language=input_lang_code, target_language=output_lang_code)
+                translation = translation['translatedText'].encode('utf-8').decode('utf-8')
+                translated_prompts.append(translation)
 
-        # Update the JSON file with the translated prompts and items
-        input_json[category]["prompts"] = translated_prompts
-        input_json[category]["items"] = translated_items
+            # Translate the items
+            translated_items = []
+            for item in items:
+                #print(item)
+                translation = translate_client.translate(item, source_language=input_lang_code, target_language=output_lang_code)
+                translation = translation['translatedText'].encode('utf-8').decode('utf-8')
+                translated_items.append(translation)
+
+            # Update the JSON file with the translated prompts and items
+            input_json[category]["prompts"] = translated_prompts
+            input_json[category]["items"] = translated_items
 
     # Save the translated JSON file
     with open(output_path, "w") as f:
         json.dump(input_json, f, indent=4)
 
 
+
 if __name__=='__main__':
-    input_path = "data\english_data.json"
-    output_path = "data\spanish_data.json"
+    input_path = "social_groups\english_data.json"
+    output_path = "social_groups\croatian_data.json"
     input_lang = Language.English
-    output_lang = Language.Spanish
+    output_lang = Language.Croatian
     ignored_words = ["<mask>", "<item>"]
 
     translate_json(input_path, output_path, input_lang, output_lang, ignored_words)
