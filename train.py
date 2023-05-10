d43c1b02815697dfb5700d9233d443463076dcae
iRaiyz <adam.nilav@gmail.com>
2023-05-10 15:36:01 +0200

    train file implemented "cleanly"

--- train.py
+++ train.py
@@ -2,14 +2,32 @@ import torch
 from transformers import XLMRobertaForMaskedLM, XLMRobertaTokenizer, DataCollatorForLanguageModeling
 from transformers import Trainer, TrainingArguments
 from datasets import load_dataset
-
-def tokenize_function(examples):
-    tokenizer = XLMRobertaTokenizer.from_pretrained("xlm-roberta-base")
-    return tokenizer(examples["text"], return_special_tokens_mask=True)
+from data import tokenize_function, preprocessing_fine_tuning
+from model import load_model, Models
+import argparse
 
 
 if __name__ == "__main__":
-    model = XLMRobertaForMaskedLM.from_pretrained("xlm-roberta-base")
+    parser = argparse.ArgumentParser(description='Multilingual Model Stereotype Analysis.')
+    parser.add_argument('--output_dir', type=str, default="./xlm-roberta-finetuned", help="Output directory for trained model.")
+    parser.add_argument('--model_name', type=str, default="xlm-roberta-base", help="Model to be fine-tuned")
+    parser.add_argument('--dataset_name', type=str, default="wikitext", help="Dataset name")
+    parser.add_argument('--dataset_version', type=str, default="wikitext-103-raw-v1", help="Dataset version")
+    parser.add_argument('--epochs', type=int, default=5)
+    parser.add_argument('--batch_size', type=int, default=64)
+
+    parser.add_argument('--verbose', action="store_true")
+    parser.add_argument('--no_output_saving', action="store_false")
+
+    args = parser.parse_args()
+
+    model_attributes = { 
+        "pipeline":"fill-mask", 
+        "top_k":200
+    }
+    model = Models(args.model_name)
+
+    model = load_model(model, model_attributes, pre_trained = True)
     # Freeze all layers
     for param in model.parameters():
         param.requires_grad = False
@@ -18,27 +36,22 @@ if __name__ == "__main__":
     for param in model.lm_head.dense.parameters():
         param.requires_grad = True
 
-    for param in model.parameters():
-        if param.requires_grad == True:
-            print(param)
+    tokenizer = XLMRobertaTokenizer.from_pretrained(args.model_name)
 
+    data_collator, tokenized_dataset = preprocessing_fine_tuning(args.dataset_name, args.dataset_version, tokenizer)
+    
 
-    tokenizer = XLMRobertaTokenizer.from_pretrained("xlm-roberta-base")
-    print("Loading dataset.")
-    dataset = load_dataset("wikitext", "wikitext-103-raw-v1", split = ["train[:5%]", "test[:1%]"])
-    tokenized_dataset = dataset[1].map(tokenize_function, batched=True, num_proc=4, remove_columns=["text"])
-    print(len(tokenized_dataset))
-
-    data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm_probability=0.15)
+    # Calculate the number of training steps per epoch
+    num_train_samples = len(tokenized_dataset["train"])
+    steps_per_epoch = num_train_samples // args.batch_size
 
     print("Training")
     training_args = TrainingArguments(
-    output_dir="./xlm-roberta-finetuned",
+    output_dir=args.output_dir,
     overwrite_output_dir=True,
-    num_train_epochs=1,
-    per_device_train_batch_size=8,
-    save_steps=10_000,
-    save_total_limit=2,
+    num_train_epochs=args.epochs,
+    per_device_train_batch_size=args.batch_size,
+    save_steps=steps_per_epoch,
     )   
 
     trainer = Trainer(
@@ -51,8 +64,8 @@ if __name__ == "__main__":
     trainer.train()
 
     print("Saving models")
-    model.save_pretrained("./xlm-roberta-finetuned")
-    tokenizer.save_pretrained("./xlm-roberta-finetuned")
+    model.save_pretrained(args.output_dir)
+    tokenizer.save_pretrained(args.output_dir)
 
 
 
