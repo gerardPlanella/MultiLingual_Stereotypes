5647b89544650b61e63e1647def39823946d1924
iRaiyz <adam.nilav@gmail.com>
2023-05-10 20:34:32 +0200

    train.py

--- train.py
+++ train.py
@@ -1,11 +1,11 @@
 import torch
 from transformers import XLMRobertaForMaskedLM, XLMRobertaTokenizer, DataCollatorForLanguageModeling
 from transformers import Trainer, TrainingArguments
-from datasets import load_dataset
+from datasets import load_dataset, Dataset
 from data import tokenize_function, preprocessing_fine_tuning
 from model import load_model, Models
 import argparse
-
+import pandas as pd
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Multilingual Model Stereotype Analysis.')
@@ -13,8 +13,8 @@ if __name__ == "__main__":
     parser.add_argument('--model_name', type=str, default="xlm-roberta-base", help="Model to be fine-tuned")
     parser.add_argument('--dataset_name', type=str, default="wikitext", help="Dataset name")
     parser.add_argument('--dataset_version', type=str, default="wikitext-103-raw-v1", help="Dataset version")
-    parser.add_argument('--epochs', type=int, default=5)
-    parser.add_argument('--batch_size', type=int, default=64)
+    parser.add_argument('--epochs', type=int, default=1)
+    parser.add_argument('--batch_size', type=int, default=8)
 
     parser.add_argument('--verbose', action="store_true")
     parser.add_argument('--no_output_saving', action="store_false")
@@ -37,12 +37,20 @@ if __name__ == "__main__":
         param.requires_grad = True
 
     tokenizer = XLMRobertaTokenizer.from_pretrained(args.model_name)
+    csv_file = "path/to/all-the-news.csv"
+    df = pd.read_csv(csv_file)
+    fox_news_df = df[df['publication'] == 'Fox News'].sample(n=5000, random_state=42)
+    fox_news_articles = fox_news_df['content'].tolist()
 
-    data_collator, tokenized_dataset = preprocessing_fine_tuning(args.dataset_name, args.dataset_version, tokenizer)
-    
+    tokenized_articles = tokenize_function(fox_news_articles)
+    fox_news_dataset = Dataset.from_dict(tokenized_articles)
+
+    # data_collator, tokenized_dataset = preprocessing_fine_tuning(args.dataset_name, args.dataset_version, tokenizer)
+    data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm_probability=0.15)
+
 
     # Calculate the number of training steps per epoch
-    num_train_samples = len(tokenized_dataset["train"])
+    num_train_samples = len(fox_news_dataset["train"])
     steps_per_epoch = num_train_samples // args.batch_size
 
     print("Training")
@@ -58,7 +66,7 @@ if __name__ == "__main__":
     model=model,
     args=training_args,
     data_collator=data_collator,
-    train_dataset=tokenized_dataset,
+    train_dataset=fox_news_dataset,
     )
 
     trainer.train()
