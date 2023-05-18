03746b20c1fb135a4ba61a1e6e3fb89c5ec4da10
gerardPlanella <gerard.plfo@gmail.com>
2023-05-18 13:09:05 +0200

    finished language comparison

--- language_analysis.py
+++ language_analysis.py
@@ -4,6 +4,30 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 from sklearn.metrics import euclidean_distances
+from sklearn.metrics.pairwise import cosine_distances
+
+def compute_similarity(matrix1, matrix2):
+    num_equal_rows = np.sum(np.all(matrix1 == matrix2, axis=1))
+    similarity = num_equal_rows / matrix1.shape[0]
+    return similarity
+
+
+def compare_all_matrices(matrix_dict):
+    names = list(matrix_dict.keys())
+    num_matrices = len(matrix_dict)
+    distances = []
+
+    for i in range(num_matrices - 1):
+        for j in range(i + 1, num_matrices):
+            name1 = names[i]
+            name2 = names[j]
+            matrix1 = matrix_dict[name1]
+            matrix2 = matrix_dict[name2]
+            distance = compute_similarity(matrix1, matrix2)#cosine_distances(matrix1, matrix2)[0, 0]
+            distances.append([name1, name2, distance])
+
+    df = pd.DataFrame(distances, columns=["Name1", "Name2", "Similarity"])
+    return df
 
 def drop_columns_with_same_value(df):
     same_value_columns = []
@@ -13,6 +37,21 @@ def drop_columns_with_same_value(df):
     df = df.drop(columns=same_value_columns)
     return df, same_value_columns
 
+def convert_to_matrices(df):
+    one_hot_dim = df.drop("Name", axis=1).to_numpy().max()
+    result = {}
+
+    for index, row in df.iterrows():
+        name = row["Name"]
+        values = row.drop("Name").values
+        one_hot_matrix = np.zeros((len(values), one_hot_dim + 1))
+
+        for i, value in enumerate(values):
+            one_hot_matrix[i, value] = 1
+
+        result[name] = one_hot_matrix
+
+    return result
 
 def convert_to_numerical(df, drop_columns=None, skip_columns:list=["Name"]):
     df_transformed = df.copy()
@@ -31,7 +70,8 @@ def convert_to_numerical(df, drop_columns=None, skip_columns:list=["Name"]):
 
     return df_transformed, value_mappings
 
-languages = ["Spanish", "English", "French", "Serbian-Croatian", "Greek (Modern)", "Japanese", "Catalan", "Italian", "German", "Portuguese", "Russian", "Swahili", "Hindi", "Mandarin"]
+languages = ["Spanish", "English", "French", "Serbian-Croatian", "Greek (Modern)", "Italian", "Catalan", "Russian", "Mandarin", "German", "Japanese", "Korean", "Hindi"]
+subset_names = ["Spanish", "English", "French", "Serbian-Croatian", "Greek (Modern)"]
 drop_columns = ["wals_code", "iso_code", "glottocode", "countrycodes", "latitude", "longitude", "macroarea"]
 
 df = pd.read_csv("data/language.csv")
@@ -54,7 +94,23 @@ print("Dropped Columns due to shared value: ")
 print(dropped_columns)
 
 
+
 """
+matrices = convert_to_matrices(df_cleaned)
+
+#Unused now
+distance_df = compare_all_matrices(matrices)
+#print(distance_df.sort_values(by=["Similarity"]))
+
+
+distance_df.to_csv('distance_data.csv', index=True)
+
+
+subset_df = distance_df[distance_df['Name1'].isin(subset_names) & distance_df['Name2'].isin(subset_names)]
+
+print(subset_df.sort_values(by=["Similarity"]))
+"""
+
 n_languages = len(df_cleaned.index)
 
 # Apply PCA to reduce the dimensions
@@ -72,90 +128,13 @@ plt.title('2-Dimension PCA for language data in WALS')
 plt.show()
 
 
+# Filter the PCA data based on the subset of names
+subset_pca_data = pca_data[df_cleaned["Name"].isin(subset_names)]
 
-# Apply PCA to reduce the dimensions to 10
-pca = PCA(n_components=5)
-pca_data = pca.fit_transform(df_cleaned.drop("Name", axis=1))
+# Compute the Euclidean distances between the subset of PCA data
+distances = euclidean_distances(subset_pca_data)
 
-# Compute Euclidean distance matrix based on 10 PCA dimensions for all languages
-distance_matrix = euclidean_distances(pca_data, pca_data)
+# Create a distance DataFrame
+distance_df = pd.DataFrame(distances, index=subset_names, columns=subset_names)
 
-# Create a list to store the results
-results = []
-
-# Iterate over the language pairs
-for i in range(len(df_cleaned)):
-    lang1 = df_cleaned["Name"].iloc[i]
-    for j in range(i+1, len(df_cleaned)):
-        lang2 = df_cleaned["Name"].iloc[j]
-        distance = distance_matrix[i, j]
-        results.append([lang1, lang2, distance])
-
-# Apply PCA to reduce the dimensions to 10
-pca = PCA(n_components=5)
-pca_data = pca.fit_transform(df_cleaned.drop("Name", axis=1))
-
-# Compute Euclidean distance matrix based on 10 PCA dimensions for all languages
-distance_matrix = euclidean_distances(pca_data, pca_data)
-
-# Create a list to store the results
-results = []
-
-# Iterate over the language pairs
-for i in range(len(df_cleaned)):
-    lang1 = df_cleaned["Name"].iloc[i]
-    for j in range(i+1, len(df_cleaned)):
-        lang2 = df_cleaned["Name"].iloc[j]
-        distance = distance_matrix[i, j]
-        results.append([lang1, lang2, distance])
-
-# Create a DataFrame from the results
-df_distance = pd.DataFrame(results, columns=["Name 1", "Name 2", "Distance"])
-pd.set_option('display.max_rows', df_distance.shape[0]+1)
-print(df_distance.sort_values(by=["Distance"]))
-"""
-
-# Define the subset of languages
-subset_languages = ["Spanish", "English", "French", "Serbian-Croatian", "Greek (Modern)", "Catalan"]
-
-# Get the indices of the subset languages
-subset_indices = [languages.index(lang) for lang in subset_languages]
-
-# Apply PCA to reduce the dimensions to 10
-pca = PCA(n_components=10)
-pca_data = pca.fit_transform(df_cleaned.drop("Name", axis=1))
-
-# Get the PCA data for the subset of languages
-subset_pca_data = pca_data[subset_indices]
-
-# Compute Euclidean distance matrix based on 10 PCA dimensions for the subset of languages
-distance_matrix = euclidean_distances(subset_pca_data, subset_pca_data)
-
-# Create a list to store the results
-results = []
-
-# Iterate over the language pairs
-for i in range(len(subset_languages)):
-    lang1 = subset_languages[i]
-    for j in range(i+1, len(subset_languages)):
-        lang2 = subset_languages[j]
-        distance = distance_matrix[i, j]
-        results.append([lang1, lang2, distance])
-
-# Create a DataFrame from the results
-df_distance = pd.DataFrame(results, columns=["Name 1", "Name 2", "Distance"])
-print(df_distance.sort_values(by=["Distance"]))
-
-# Apply PCA to reduce the dimensions for the subset of languages
-pca = PCA(n_components=2)
-subset_pca_data = pca.fit_transform(df_cleaned[df_cleaned["Name"].isin(subset_languages)].drop("Name", axis=1))
-
-# Create a scatter plot for the subset of languages
-plt.figure(figsize=(8, 6))
-for i, language in enumerate(subset_languages):
-    plt.scatter(subset_pca_data[i, 0], subset_pca_data[i, 1], c='blue', marker='o')
-    plt.annotate(language, (subset_pca_data[i, 0], subset_pca_data[i, 1]), textcoords="offset points", xytext=(0,10), ha='center')
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.title('2-Dimension PCA for subset of languages in WALS')
-plt.show()
+print(distance_df)
