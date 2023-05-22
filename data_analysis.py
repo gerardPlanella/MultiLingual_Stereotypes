bb546646f107afb3a52c95202ddad30650459185
iRaiyz <adam.nilav@gmail.com>
2023-05-22 18:28:26 +0200

    data analysis

--- data_analysis.py
+++ data_analysis.py
@@ -1,6 +1,8 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
+import os
+import json
 
 def find_stats(pre_train_df, fine_tune_df, n = 5):
     
@@ -22,37 +24,50 @@ def find_stats(pre_train_df, fine_tune_df, n = 5):
         
     return dic
 
+def get_second_elements(root_folder):
+    output_dict = {}
+
+    # walk through all subdirectories and files in the directory
+    for dirpath, dirnames, filenames in os.walk(root_folder):
+
+        # create a list for current directory
+        dir_list = []
+
+        # iterate over all files
+        for filename in filenames:
+            # check if file is a JSON file
+            if filename == 'race.json':
+                # construct full file path
+                file_path = os.path.join(dirpath, filename)
+
+                # load JSON file
+                with open(file_path, 'r') as file:
+                    data = json.load(file)
+
+                    # check if data is a list and has at least two elements
+                    if isinstance(data, list) and len(data) >= 2:
+                        # append second element to dir_list
+                        dir_list.append(data[1])
+
+        # add dir_list to output_dict with folder name as key if it's not empty
+        if dir_list:
+            folder_name = os.path.basename(dirpath)
+            output_dict[folder_name] = dir_list
+
+    # convert output_dict to DataFrame
+    output_df = pd.DataFrame.from_dict(output_dict, orient='index')
+
+    return output_df
+
+def row_means(dataframe):
+    # Compute mean for each row, ignore NaN values
+    means = dataframe.mean(axis=1, skipna=True)
+
+    # Convert to list
+    means_list = means.tolist()
+
+    return means_list
+
 if __name__ == "__main__":
-    racial_minorities =  ["Indians", "Russians", "Africans", "Black people",
-        "Mexicans", "Blacks","Arabs",
-        "Black Americans", "Black men",
-        "Asian women", "Asian men", "Black women", 
-        "Indian men", "Indian women", "Chinese men", "Chinese women",
-        "Indian parents", "Asian parents", "Black fathers", "Latinas", "Latinos",
-        "Asian kids", "Black kids", "African Americans",
-        "Nigerians", "Ethiopians","Sudanese people", "Afghans", "Iraqis",
-        "Somalis", "Iranian people", "Iranians",
-        "Ghanaians",
-        "Syrians", "Pakistanis",
-        "Romanians", "Ecuadorians",
-        "Turkish people"
-        ]
-    
-    df_1 = pd.read_csv('out\pretrained_roberta\emotion_profiles\\english\\race.csv', index_col = False)
-    df_1.rename( columns={'Unnamed: 0':'group'}, inplace=True )
-    print(df_1)
-    df = df_1[df_1.index.isin(racial_minorities)]
-    print(df)
-    # df_2 = pd.read_csv('out\\finetuned_stereoset_english\emotion_profiles\\French\\race.csv', index_col = False)
-    # df_2.rename( columns={'Unnamed: 0':'group'}, inplace=True )
-
-    # print(find_stats(df_1, df_2))
-
-    # df_1 = pd.read_csv('out\\finetuned_stereoset_english\emotion_profiles\English\\race.csv', index_col = False)
-    # df_1.rename(columns={'Unnamed: 0':'group'}, inplace=True )
-    # df_1.set_index('group', inplace=True)
-
-    # print(df_1.head())
-    # plt.figure(figsize=(10, 10))
-    # sns.heatmap(df_1, cmap='viridis')
-    # plt.show()
+    print(get_second_elements('out\\greek_finetune\spearman_correlations_RSA'))
+    # print(row_means(get_second_elements('out\\spanish_finetune\spearman_correlations_RSA')))
