7fcef7329b876784188ae77c20006a981150930c
Valin <adam.nilav@gmail.com>
2023-06-04 19:42:50 +0200

    no main model.py

--- model.py
+++ model.py
@@ -45,12 +45,3 @@ def load_model(model:Models, model_attributes:dict = {}, model_name = 'base'):
     else:
         raise Exception(f"Model {model.value} not implemented.")
     return model
-
-if __name__ == "__main__":
-    model = Models('xlm-roberta-base')
-    model_attributes = { 
-        "pipeline":"fill-mask", 
-        "top_k":10
-    }
-    unmasker = load_model(model, model_attributes, model_name = 'french')
-    print(unmasker("I am <mask>."))
