65c3638a79b7ea2ee34f88a9644a704084c1fbc6
Valin <adam.nilav@gmail.com>
2023-06-04 20:04:05 +0200

    run_corr

--- run_correlations.py
+++ run_correlations.py
@@ -17,9 +17,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Multilingual Model Stereotype Analysis.')
     parser.add_argument('--social_groups', nargs='+', default=social_groups, help="Social Groups to Analyse.")
     parser.add_argument('--language_1_path', type=str, default="social_groups/english_data.json", help="Language 1 to analyse.")
+    parser.add_argument('--language_2_path', type=str, default="social_groups/croatian_data.json", help="Language 2 to analyse.")   
     parser.add_argument('--output_dir', type=str, default="out/french_finetune", help="Output directory for generated data.")
     parser.add_argument('--model_name', type=str, default="xlm-roberta-base", help="Model Evaluated")
-
+    parser.add_argument('--model_top_k', type=int, default=300, help="Top K results used for matrix generation.")   
     args = parser.parse_args()
 
     args.language_1 = os.path.basename(args.language_1_path).split("_")[0]
